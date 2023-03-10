76427d10e658f0e4d2b867eff6d189091ce85e27
Olga-cyber84 <doorsdream007@ya.ru>
2023-03-10 17:41:23 +0300

    comments were deleted

--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect
 from phones.models import Phone
-from django.http import HttpResponse
 
 
 def index(request):
@@ -11,7 +10,6 @@ def show_catalog(request):
     template = 'catalog.html'
     phone_object = Phone.objects.all()
     if request.GET.get('sort'):
-        # return HttpResponse('ok - ok')
         if request.GET.get('sort') == 'name':
             phone_object = Phone.objects.order_by('name')
         if request.GET.get('sort') == 'min_price':
@@ -20,7 +18,6 @@ def show_catalog(request):
             phone_object = Phone.objects.order_by('-price')
     context = {"phones": phone_object}
     return render(request, template, context)
-    # return HttpResponse('ok')
 
 
 def show_product(request, slug):
