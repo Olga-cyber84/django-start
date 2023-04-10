b068ee03adb534b0a75ace78782c2ddc691548cb
Olga-cyber84 <doorsdream007@ya.ru>
2023-04-10 17:44:21 +0300

    some changes was added, but intermediate model is empty

--- 2.2-databases-2/m2m-relations/articles/models.py
+++ 2.2-databases-2/m2m-relations/articles/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 
 class Article(models.Model):
-
     title = models.CharField(max_length=256, verbose_name='Название')
     text = models.TextField(verbose_name='Текст')
     published_at = models.DateTimeField(verbose_name='Дата публикации')
-    image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
+    image = models.ImageField(null=True, blank=True,
+                              verbose_name='Изображение',)
 
     class Meta:
         verbose_name = 'Статья'
@@ -14,3 +14,23 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Название')
+    articles = models.ManyToManyField(
+        Article, related_name='tags', through='Scope')
+
+    class Meta:
+        verbose_name = 'Тематика'
+        verbose_name_plural = 'Тематики'
+
+    def __str__(self):
+        return self.name
+
+
+class Scope(models.Model):
+    article = models.ForeignKey(
+        Article, related_name='scopes', on_delete=models.CASCADE)
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    is_main = models.BooleanField(default=False)
