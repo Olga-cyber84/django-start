9632fd1354b65b1ebe3ef2b3f49e486a922d4681
Olga-cyber84 <doorsdream007@ya.ru>
2023-05-13 23:54:25 +0300

    first commit

--- 3.1-drf-intro/smart_home/measurement/models.py
+++ 3.1-drf-intro/smart_home/measurement/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # TODO: опишите модели датчика (Sensor) и измерения (Measurement)
+
+
+class Sensor(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=100)
+
+
+class Measurement:
+    id = models.IntegerField(primary_key=True)
+    temperature = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now=True)
--- 3.1-drf-intro/smart_home/measurement/views.py
+++ 3.1-drf-intro/smart_home/measurement/views.py
@@ -1,2 +1,17 @@
 # TODO: опишите необходимые обработчики, рекомендуется использовать generics APIView классы:
 # TODO: ListCreateAPIView, RetrieveUpdateAPIView, CreateAPIView
+from rest_framework.decorators import api_view
+from rest_framework.decorators import Response
+
+
+@api_view
+def ListCreateAPIView():
+    pass
+
+
+def RetrieveUpdateAPIView():
+    pass
+
+
+def CreateAPIView():
+    pass
